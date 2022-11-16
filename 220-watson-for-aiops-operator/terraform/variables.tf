60d19c368b0e66dc7a1bd95ed1cad6b7fe4e4e38
Chechu <chechu@ChechusLaptop.local>
2022-11-16 12:45:13 +0100

    added ocp-virt code to provision vm

--- 220-watson-for-aiops-operator/terraform/variables.tf
+++ 220-watson-for-aiops-operator/terraform/variables.tf
@@ -1,8 +1,8 @@
-variable "cp4i-version-dependency_cp4i_version" {
-  type = string
-  description = "The CP4i Version. Possible values are (2021_4_1 , 2022_1_1). If no values is set by default this will refer to Latest available CP4i version"
-  default = "2022_2_1"
-}
+# variable "cp4i-version-dependency_cp4i_version" {
+#   type = string
+#   description = "The CP4i Version. Possible values are (2021_4_1 , 2022_1_1). If no values is set by default this will refer to Latest available CP4i version"
+#   default = "2022_2_1"
+# }
 variable "gitops-cp-catalogs_namespace" {
   type = string
   description = "The namespace where the application should be deployed"
