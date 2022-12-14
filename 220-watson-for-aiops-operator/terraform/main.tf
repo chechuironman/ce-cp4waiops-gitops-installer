f518a4b6f70d181eb07954791766fc6d44e6b294
Chechu <chechu@ChechusLaptop.local>
2022-12-14 23:38:24 +0100

    added waiops

--- 220-watson-for-aiops-operator/terraform/main.tf
+++ 220-watson-for-aiops-operator/terraform/main.tf
@@ -77,7 +77,7 @@ module "gitops-cp-catalogs" {
   server_name = module.gitops_repo.server_name
 }
 module "gitops-cp-waiops" {
-  source = "github.com/chechuironman/terraform-gitops-cp-waiops-operator"
+  source = "https://github.com/cloud-native-toolkit/terraform-gitops-waiops"
 
   catalog = module.gitops-cp-catalogs.catalog_ibmoperators
   catalog_namespace = var.gitops-cp-waiops_catalog_namespace
